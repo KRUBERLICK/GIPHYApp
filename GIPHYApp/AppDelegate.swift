4a8b655c994896ce80b0457e6ced9815abac2a95
Daniel Ilchishyn <kruberlick@gmail.com>
2017-11-02 11:25:20 +0200

    Initial commit

---
+++ GIPHYApp/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  GIPHYApp
+//
+//  Created by Daniel Ilchishyn on 11/2/17.
+//  Copyright © 2017 KRUBERLICK. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ GIPHYApp/BrowseGIFsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BrowseGIFsViewController.swift
+//  GIPHYApp
+//
+//  Created by Daniel Ilchishyn on 11/2/17.
+//  Copyright © 2017 KRUBERLICK. All rights reserved.
+//
+
+import UIKit
+
+class BrowseGIFsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+    }
+}
